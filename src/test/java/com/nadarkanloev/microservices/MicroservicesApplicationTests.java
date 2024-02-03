bac84a0e2f349828783e6fc4d4b233608b75ebbd
Nadar <donat290817@gmail.com>
2024-02-04 02:26:44 +0300

    Создание репозитория

---
+++ src/test/java/com/nadarkanloev/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadarkanloev.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+
+}
